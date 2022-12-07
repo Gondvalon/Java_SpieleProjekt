9341468be033acc0cb13769a339339fbb293bd1b
micha <michaelzscherp@gmail.com>
2022-12-07 14:33:31 +0100

    MainMenu experimental

--- Brotato/src/Game/MainMenuState.java
+++ Brotato/src/Game/MainMenuState.java
@@ -1,11 +1,26 @@
 package Game;
 
+import eea.engine.action.Action;
+import eea.engine.action.basicactions.ChangeStateInitAction;
+import eea.engine.action.basicactions.QuitAction;
+import eea.engine.component.Component;
+import eea.engine.component.RenderComponent;
+import eea.engine.component.render.ImageRenderComponent;
+import eea.engine.entity.Entity;
 import eea.engine.entity.StateBasedEntityManager;
+import eea.engine.event.ANDEvent;
+import eea.engine.event.basicevents.MouseClickedEvent;
+import eea.engine.event.basicevents.MouseEnteredEvent;
+
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+
+import de.tud.gdi1.dropofwater.ui.Launch;
 
 public class MainMenuState extends BasicGameState {
 
@@ -22,7 +37,68 @@ public class MainMenuState extends BasicGameState {
 
     @Override
     public void init(GameContainer container, StateBasedGame game) throws SlickException {
-
+    	// Hintergrund laden
+    	Entity background = new Entity("menu");	// Entitaet fuer Hintergrund
+    	background.setPosition(new Vector2f(0,0));	// Startposition des Hintergrunds
+    	background.addComponent(new RenderComponent("background") {
+			
+			@Override
+			public void update(GameContainer arg0, StateBasedGame arg1, int arg2) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2) {
+				
+				arg2.fillRect(0, 0, 1000, 1000);
+				
+			}
+			
+			@Override
+			public Vector2f getSize() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		}); // Bildkomponente
+    	    	
+    	// Hintergrund-Entitaet an StateBasedEntityManager uebergeben
+    	entityManager.addEntity(stateID, background);
+    	
+    	/* Neues Spiel starten-Entitaet */
+    	String new_Game = "Neues Spiel starten";
+    	Entity new_Game_Entity = new Entity(new_Game);
+    	
+    	// Setze Position und Bildkomponente
+    	new_Game_Entity.setPosition(new Vector2f(218, 190));
+    	new_Game_Entity.setScale(0.28f);
+    	new_Game_Entity.addComponent(new ImageRenderComponent(new Image("assets/entry.png")));
+    	
+    	// Erstelle das Ausloese-Event und die zugehoerige Action
+    	ANDEvent mainEvents = new ANDEvent(new MouseEnteredEvent(), new MouseClickedEvent());
+    	Action new_Game_Action = new ChangeStateInitAction(Launch.GAMEPLAY_STATE);
+    	mainEvents.addAction(new_Game_Action);
+    	new_Game_Entity.addComponent(mainEvents);
+    	
+    	// Fuege die Entity zum StateBasedEntityManager hinzu
+    	entityManager.addEntity(this.stateID, new_Game_Entity);
+    	
+    	/* Beenden-Entitaet */
+    	Entity quit_Entity = new Entity("Beenden");
+    	
+    	// Setze Position und Bildkomponente
+    	quit_Entity.setPosition(new Vector2f(218, 290));
+    	quit_Entity.setScale(0.28f);
+    	quit_Entity.addComponent(new ImageRenderComponent(new Image("assets/entry.png")));
+    	
+    	// Erstelle das Ausloese-Event und die zugehoerige Action
+    	ANDEvent mainEvents_q = new ANDEvent(new MouseEnteredEvent(), new MouseClickedEvent());
+    	Action quit_Action = new QuitAction();
+    	mainEvents_q.addAction(quit_Action);
+    	quit_Entity.addComponent(mainEvents_q);
+    	
+    	// Fuege die Entity zum StateBasedEntityManager hinzu
+    	entityManager.addEntity(this.stateID, quit_Entity);
     }
 
     public void update(GameContainer container, StateBasedGame game, int delta)
@@ -33,6 +109,12 @@ public class MainMenuState extends BasicGameState {
     public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
         // StatedBasedEntityManager soll alle Entities rendern
         entityManager.renderEntities(container, game, g);
+        
+		int counter = 0;
+
+		g.drawString("Neues Spiel", 110, start_Position+counter*distance); counter++;
+		g.drawString("Optionen", 110, start_Position+counter*distance); counter++;
+		g.drawString("Beenden", 110, start_Position+counter*distance); counter++;
     }
 
     @Override
